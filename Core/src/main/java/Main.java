1f8828477e5e8f377a2e3ef904a845e2bfee3ea6
NovaAnte <andreas_cabraja@hotmail.com>
2021-06-03 15:02:31 +0200

    URL handling

--- Core/src/main/java/Main.java
+++ Core/src/main/java/Main.java
@@ -17,18 +17,16 @@ public class Main {
     public static void main(String[] args) {
 
 
-
         ExecutorService executorService = Executors.newCachedThreadPool();
 
-        try(ServerSocket serverSocket = new ServerSocket(80)) {
+        try (ServerSocket serverSocket = new ServerSocket(80)) {
 
-            while(true) {
+            while (true) {
                 Socket client = serverSocket.accept();
                 executorService.submit(() -> handleConnection(client));
             }
 
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
 
             e.printStackTrace();
         }
@@ -39,23 +37,41 @@ public class Main {
         try {
 
             BufferedReader inputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            String url = readRequest(inputFromClient);
+            String inputHeader = readRequest(inputFromClient);
+            String type = inputHeader.split(" ")[0];
+            String url = inputHeader.split(" ")[1];
 
             OutputStream outputToClient = client.getOutputStream();
 
-            if(url.equals("/cat.png"))
-                sendImageResponse(outputToClient);
-            else if(url.equals("/dog.jpg"))
-                sendDogImageResponse(outputToClient);
-            else
-                sendJsonResponse(outputToClient);
+
+            if (type.equals("GET")) {
+                switch (url) {
+                    case "/cat.png":
+                        sendImageResponse(outputToClient);
+                        break;
+                    case "/dog.png":
+                        sendDogImageResponse(outputToClient);
+                        break;
+                    default:
+                        sendJsonResponse(outputToClient);
+                        break;
+                }
+            } else if (type.equals("HEAD")) {
+                switch (url) {
+
+                }
+
+            } else if (type.equals("POST")) {
+                switch (url) {
+
+                }
+            }
 
             inputFromClient.close();
             outputToClient.close();
             client.close();
 
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -66,18 +82,17 @@ public class Main {
         byte[] data = new byte[0];
 
         File f = Path.of("Core", "target", "web", "dog.jpg").toFile();
-        if(!f.exists() && !f.isDirectory()) {
+        if (!f.exists() && !f.isDirectory()) {
             header = "HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n";
         } else {
 
-            try(FileInputStream fileInputStream = new FileInputStream(f)) {
+            try (FileInputStream fileInputStream = new FileInputStream(f)) {
 
-                data = new byte[(int)f.length()];
+                data = new byte[(int) f.length()];
                 fileInputStream.read(data);
                 String contentType = Files.probeContentType(f.toPath());
                 header = "HTTP/1.1 200 OK\r\nContent-Type: " + contentType + "\r\nContent-length: " + data.length + "\r\n\r\n";
-            }
-            catch(IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
@@ -90,22 +105,21 @@ public class Main {
 
     private static void sendImageResponse(OutputStream outputToClient) throws IOException {
 
-        String header  ="";
+        String header = "";
         byte[] data = new byte[0];
 
         File f = Path.of("Core", "target", "web", "cat.png").toFile();
-        if(!f.exists() && !f.isDirectory()) {
+        if (!f.exists() && !f.isDirectory()) {
             header = "HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n";
         } else {
 
-            try(FileInputStream fileInputStream = new FileInputStream(f)) {
+            try (FileInputStream fileInputStream = new FileInputStream(f)) {
 
-                data = new byte[(int)f.length()];
+                data = new byte[(int) f.length()];
                 fileInputStream.read(data);
                 String contentType = Files.probeContentType(f.toPath());
                 header = "HTTP/1.1 200 OK\r\nContent-Type: " + contentType + "\r\nContent-length: " + data.length + "\r\n\r\n";
-            }
-            catch(IOException e) {
+            } catch (IOException e) {
 
                 e.printStackTrace();
             }
@@ -136,16 +150,25 @@ public class Main {
 
     private static String readRequest(BufferedReader inputFromClient) throws IOException {
 
+        String type = "";
         String url = "";
 
-        while(true) {
-
+        while (true) {
             String line = inputFromClient.readLine();
-            if(line.startsWith("GET"))
+            if (line.startsWith("GET")) {
+                type = "GET";
                 url = line.split(" ")[1];
-            if(line == null || line.isEmpty())
-                break;
+
+            } else if (line.startsWith("HEAD")) {
+                type = "HEAD";
+                url = line.split(" ")[1];
+
+            } else if (line.startsWith("POST")) {
+                type = "POST";
+                url = line.split(" ")[1];
+            }
+            return type + " " + url;
         }
-        return url;
     }
+
 }
